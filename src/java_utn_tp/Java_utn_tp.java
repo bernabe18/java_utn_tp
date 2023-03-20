22fc600750d897ccb75b4fd5c2260c1d5dd75d2b
LeandroVallejos <leanvallejos666@gmail.com>
2023-03-20 13:05:05 -0300

    clase 1 tp

--- src/java_utn_tp/Java_utn_tp.java
+++ src/java_utn_tp/Java_utn_tp.java
@@ -1,17 +1,39 @@
-
 package java_utn_tp;
 
-/**
- *
- * @author usuario
- */
 public class Java_utn_tp {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+// a. Utilizando la sentencia while, realice un ejemplo que muestre los números del 1 al 
+//100 utilizando la instrucción while. Y luego, que imprima los mismos por pantalla
+//    int num=0;
+//        while (num<100) {
+//          num++;
+//            System.out.println(num);
+//        }
+
+ //b. A lo anterior, que muestre los números del 1 al 100 utilizando la instrucción for.       
+//        for (int i=1;i<101;i++) {
+//            System.out.println(i);
+//        }
+//        
+        
+//c. A lo anterior, con la sentencia do while
+//int num=0;
+//do {            
+//    num++;
+//    System.out.println(num);
+//    
+//} while (num<100);
+//        
+        
+//d. Utilizando la sentencia for, hacer lo mismo que en (b) pero que el valor a imprimir por 
+//pantalla, sea el doble.
+//for (int i = 1; i < 101; i++) {
+//            System.out.println(i*2);
+//        }
+        
+
+        
     }
-    
+
 }
