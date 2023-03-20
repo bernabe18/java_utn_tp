3a8e1efa014af8dbcce9abfbd4b3d64398056ad4
LeandroVallejos <leanvallejos666@gmail.com>
2023-03-20 12:48:38 -0300

    creando repo

---
+++ src/java_utn_tp/Java_utn_tp.java
@@ -0,0 +1,17 @@
+
+package java_utn_tp;
+
+/**
+ *
+ * @author usuario
+ */
+public class Java_utn_tp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
